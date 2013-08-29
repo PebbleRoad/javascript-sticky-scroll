dd883cf9bb74468b000008819bf97870c574f629
Vinay <rmdort@gmail.com>
2013-08-29 18:15:03 +0800

    updated sticky scroll

--- sticky-scroll.js
+++ sticky-scroll.js
@@ -1,25 +1,49 @@
-(function(){
+(function(window, document){
 
-  var stickyScroll = function(opts){
+  /**
+   * Default Options
+   */
   
-    this.settings = {};
+  var defaultOptions = {
+    stickyklass   : 'block--sticky',
+    element : '.block--sticyScroll',
+    bounds  : '.block--body',
+    gutter  : 20
+  }
 
-    if(typeof opts == "undefined"){
+  /**
+   * Constructor
+   */
 
-      this.settings.klass   = 'block--sticky';
-      this.settings.ele     = '.block--sticyScroll';
-      this.settings.bounds  = '.block--body';
-      this.settings.gutter  = 20;
-    }
-    else{
-      this.settings = opts;
-    }
-    
+  function StickyScroll(options){
+  
+    this.settings = this.extend({}, defaultOptions, options);    
 
   };
   
 
-  stickyScroll.prototype.getPos = function(ele) {
+  /*
+  * Extend
+  */
+
+  StickyScroll.prototype.extend = function(){
+   if (arguments.length > 1) {
+     var master = arguments[0];
+     for (var i = 1, l = arguments.length; i < l; i++) {
+       var object = arguments[i];
+       for (var key in object) {
+         master[key] = object[key];
+       }
+     }
+   }
+   return master;
+  };
+
+
+  /**
+   * Get X,Y Position of an element
+   */
+  StickyScroll.prototype.getPos = function(ele) {
         var x=0;
         var y=0;
         while(true){
@@ -35,24 +59,28 @@
         }
   };
     
-  stickyScroll.prototype.getHeight = function(ele){
+  /**
+   * Gets Height of an Element with margin bottom and top
+   */
+  StickyScroll.prototype.getHeight = function(ele){
       
       return ele.offsetHeight + 
-            Number(window.getComputedStyle(ele)['marginTop'].replace(/[^\d\.]/g, '')) + 
-            Number(window.getComputedStyle(ele)['marginBottom'].replace(/[^\d\.]/g, ''));
+            Number(window.getComputedStylePropertyValue(ele, 'margin-top').replace(/[^\d\.]/g, '')) + 
+            Number(window.getComputedStylePropertyValue(ele,'margin-bottom').replace(/[^\d\.]/g, ''));
       
-    },
+  };
     
-  stickyScroll.prototype.init =function(){
+  StickyScroll.prototype.init =function(){
       
       var self = this;
-      this.ele = document.querySelector(this.settings.ele); 
+      
+      this.ele = document.querySelector(this.settings.element);       
       
       if(this.ele) this.setDefaults();
       
-    },
+  };
     
-  stickyScroll.prototype.setDefaults = function(){
+  StickyScroll.prototype.setDefaults = function(){
       
       var self              = this;
       this.eleWidth         = this.ele.clientWidth;           
@@ -64,34 +92,67 @@
       
       this.ele.style.width = this.eleWidth + 'px';
       
-      window.addEventListener("scroll", function(){
+      window.addEventListener("scroll", function(){        
+        
         self.scrollHandler()
+
       }, false);
       
       
-    },
+  };
     
-  stickyScroll.prototype.scrollHandler = function(e){
+  StickyScroll.prototype.scrollHandler = function(e){
       
-      var windowScroll = window.pageYOffset || window.scrollTop;            
+      var windowScroll = window.pageYOffset || window.scrollTop || document.documentElement.scrollTop;            
     
       
       /* Check if element is below the page Viewport */     /* Check if element is within the bounds */    
       if(windowScroll > this.eleOffsetTop && windowScroll < (this.containerHeight + this.containerTop - this.eleHeight - this.settings.gutter)) {
         
-        this.ele.classList.add(this.settings.klass);
+        this.ele.classList.add(this.settings.stickyklass);
       }else{
-        this.ele.classList.remove(this.settings.klass);
+        this.ele.classList.remove(this.settings.stickyklass);
       }
       
     
   };
 
-  window.addEventListener('load', function(){
-    var sticky = new stickyScroll();
+  /**
+   * Assign to Window Object   
+   */
+  
+  window.StickyScroll = StickyScroll;
+
+
+  /**
+   * Get Computer Style Shim
+   * https://developer.mozilla.org/en-US/docs/Web/API/window.getComputedStyle
+   */
     
-    sticky.init();
-  })
+  window.getComputedStylePropertyValue = function(el,cssProperty){
+          if(!window.getComputedStyle){
+              if(document.defaultView && document.defaultView.getComputedStyle){
+                  return document.defaultView.getComputedStyle.getPropertyValue(cssProperty);
+              }    
+              else{
+                      var camelCasedCssProperty = getCamelCasedCssProperty(cssProperty);
+                      if(el.currentStyle){
+                          return el.currentStyle(camelCasedCssProperty);
+                      }
+                      else{
+                          return el.style[camelCasedCssProperty];
+                      }
+              }
+          }
+          else{
+                  return window.getComputedStyle(el).getPropertyValue(cssProperty);
+          }
+          
+  }
+
+  function getCamelCasedCssProperty(cssProperty){
+          return cssProperty.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase() });
+  }  
 
 
-})()
+})(window, document, undefined);
